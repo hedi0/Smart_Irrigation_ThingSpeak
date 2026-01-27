fef7aeb7f1b8bc0a457ef8c2660e24f4aa83128f
Med Hedi Abd <102860383+hedi0@users.noreply.github.com>
2026-01-27 17:13:06 +0100

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -1,1 +1,19 @@
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <ThingSpeak.h>
 
+#define soil_pin A0
+#define relay_pin D1
+#define DHTPIN D4
+DHT dht(DHTPIN, DHT22);
+
+//ThingSpeak credentials
+String apiKey = "THINGSPEAK_WRITE_API_KEY"; // example : 
+const char* server = "api.thingspeak.com";
+
+const char* ssid =  "PASTE_YOUR_SSID_HERE";
+const char* pass =  "PASTE_YOUR_PASSWORD_HERE";
+
+WiFiClient client;
+
+int threshholdDRY = 800;
