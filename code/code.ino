3108dbed7125098007b9ed1116ad137ace5b92d2
Med Hedi Abd <102860383+hedi0@users.noreply.github.com>
2026-01-27 17:42:29 +0100

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -5,7 +5,9 @@
 #define soil_pin A0
 #define relay_pin D1
 #define DHTPIN D4
-DHT dht(DHTPIN, DHT22);
+
+#define soilDRY 310
+#define soilWET 750
 
 //ThingSpeak credentials
 const char* WriteAPIkey = "THINGSPEAK_WRITE_API_KEY"; // example : CVC8CV4VC8L4VCVC
@@ -15,8 +17,7 @@ const char* ssid =  "PASTE_YOUR_SSID_HERE";
 const char* pass =  "PASTE_YOUR_PASSWORD_HERE";
 
 WiFiClient client;
-
-int threshholdDRY = 800;
+DHT dht(DHTPIN, DHT22);
 
 void setup() {
   Serial.begin(9600);
@@ -38,7 +39,11 @@ void setup() {
 }
 
 void loop(){
-  int moisturevalue = analogRead(soil_pin);
+  int soilmoistureValue = analogRead(soil_pin);
+  // change the analog read value from number to percentage 
+  int moistpercentage = map(soilmoistureValue,soilDRY,soilWET,0,100);
+  moistpercentage = constrain(moistpercentage,0,100);
+  
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
 
@@ -46,5 +51,11 @@ void loop(){
     Serial.println("Failed to read from Sensor(DHT22) ");
     return;
   }
-  
+
+  if(moistpercentage < 40){
+    digitalWrite(relay_pin, LOW);
+    Serial.println("Pump Active");
+  }else{
+    
+  }
 }
